45c776676fd27f7a72f47b89469e2a67e8f545f0
Cloud User <centos@ip-172-31-44-72.us-east-2.compute.internal>
2020-11-09 17:50:13 +0000

    commit by sheetal didi

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World from ankit!";
+    private final String message = "Hello World from sheetal didi!";
 
     public App() {}
 
